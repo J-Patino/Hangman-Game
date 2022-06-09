ba7215470cb36230299f51958da7b51bb521bd48
Jose E Patino <106132205+J-Patino@users.noreply.github.com>
2022-06-09 04:36:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+#Step 5
+import replit
+import random
+import hangman_words
+import hangman_art
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    #Use the clear() function imported from replit to clear the output between guesses.
+    replit.clear()
+  
+    if guess in display:
+        print(f"You have already guessed {guess}")
+                                         
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+      
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that is not in the word, you lose a life.")
+  
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if not "_" in display:
+        end_of_game = True
+        print("You win.")
+
+ 
+    print(hangman_art.stages[lives])
